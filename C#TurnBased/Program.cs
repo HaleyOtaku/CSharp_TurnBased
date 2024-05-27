5441f0b250ecd59c6beec3a220d98eb23f64fccd
HaleyOtaku <haleyotaku@gmail.com>
2024-05-27 11:19:06 -0500

    Add project files.

---
+++ C#TurnBased/Program.cs
@@ -0,0 +1,12 @@
+﻿//Following this tutorial => https://youtu.be/30NsBqPNMzU?si=FLOuY19MjBGOcfva
+
+namespace C_TurnBased
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
